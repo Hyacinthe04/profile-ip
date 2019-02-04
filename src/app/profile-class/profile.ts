3a98305b78c9b134af3842502e87227d137220f6
Hyacinthe04 <hyacinthen04@gmail.com>
2019-02-04 16:46:29 +0200

    http

---
+++ src/app/profile-class/profile.ts
@@ -0,0 +1,3 @@
+export class Profile {
+    constructor(public login:string,  public avatar_url:string,public following:number,public followers:number,public public_repos:number,public location:string, public html_url:string ){}
+}location
