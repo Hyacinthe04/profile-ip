1d248eb538c0ffb53530401e52c58f2dc0d7ff6e
Hyacinthe04 <hyacinthen04@gmail.com>
2019-02-06 10:03:40 +0200

    css

--- src/app/profile-class/profile.ts
+++ src/app/profile-class/profile.ts
@@ -1,3 +1,3 @@
 export class Profile {
-    constructor(public login:string,  public avatar_url:string,public following:number,public followers:number,public public_repos:number,public location:string, public html_url:string ){}
+    constructor(public login:string,  public avatar_url:string,public following:number,public followers:number,public public_repos:number,public location:string, public html_url:string ,public completeDate:Date){}
 }location
